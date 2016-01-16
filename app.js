915375ae0d621b909808e4badb46e317b5bf27a2
Kenandrae Wheatt <kwheatt@catalystitservices.com>
2016-01-15 16:15:42 -0800

    refactor of indexhtml

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+console.log('Listening at Port ' + port + '...');
+
