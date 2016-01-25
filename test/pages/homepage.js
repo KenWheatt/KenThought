12463c4d043380b6874501fbc2f848b452911003
Kenandrae Wheatt <kwheatt@catalystitservices.com>
2016-01-25 15:49:09 -0800

    testing for navbar as complete as I want it to be

--- test/pages/homepage.js
+++ test/pages/homepage.js
@@ -1,8 +1,8 @@
 var HomePage = function() {
     
-      this.linkedInLink = element(by.id('linkedBut'));
-      this.faceBookLink = element(by.id('faceBut'));
-
+       this.linkedInLink = element(by.id('linkedBut'));
+       this.faceBookLink = element(by.id('faceBut'));
+       this.hamburgDrop = element(by.id('hamBut'));
 };
 
 module.exports = new HomePage();
--- test/spec.js
+++ test/spec.js
@@ -1,14 +1,11 @@
- var homepage = require('./pages/homepage.js');
-
-
-console.log(homepage);
-
-describe('Homepage for KenThought', function() {
-   
+ describe('Homepage for KenThought', function() {
     
-    browser.ignoreSynchronization = true;
+          var homepage = require('./pages/homepage.js');
+     
+               // console.log(homepage);
     
     beforeEach(function(){
+         browser.ignoreSynchronization = true;
         browser.get('http://localhost:3000');
     });
     
@@ -16,7 +13,27 @@ describe('Homepage for KenThought', function() {
         expect(browser.getTitle()).toEqual('KenThought');
     });
     
-    it('should have links to linkedIn and facebook', function() {
+    it('should have link to linkedIn', function() {
         expect(homepage.linkedInLink.isPresent()).toBe(true);
+        homepage.linkedInLink.click();
+        browser.sleep(2000);
+        var newName = browser.driver.getCurrentUrl();
+        expect(newName).toEqual('https://www.linkedin.com/in/kenwheatt');
+    });
+     
+     it('should have link to facebook', function() {
+        expect(homepage.faceBookLink.isPresent()).toBe(true);
+        homepage.faceBookLink.click();
+        browser.sleep(2000);
+        var newName = browser.driver.getCurrentUrl();
+        expect(newName).toEqual('https://www.facebook.com/kenandrae.wheatt');
+    });
+     
+     it('should have a title', function() {
+         var width = 656;
+         var height = 517;
+         browser.driver.manage().window().setSize(width, height);
+         expect(homepage.hamburgDrop.isPresent()).toBe(true);
+         browser.sleep(2000);
     });
 });
